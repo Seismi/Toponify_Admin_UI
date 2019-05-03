85d707ae7fd9d1c8633c26e88b51c2bd314709e2
EdvaLu <edvardaslunys@gmail.com>
2019-05-03 12:09:03 +0100

    update endpoint

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
         pathMatch: 'full'
       },
       {
-        path: 'workpackage',
+        path: 'workpackages',
         loadChildren: './workpackage/workpackage.module#WorkpackageModule'
       },
       {
