47ec446b1ca0df38d4cb60bd8bcb6a8263cbb307
EdvaLu <edvardaslunys@gmail.com>
2019-06-17 11:42:35 +0100

    replace users list to table

--- src/app/settings/components/users-list/users-list.component.ts
+++ src/app/settings/components/users-list/users-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { User } from '@app/settings/store/models/user.model';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { TeamDetails } from '@app/settings/store/models/team.model';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
     selector: 'smi-users-list',
@@ -8,11 +9,22 @@ import { User } from '@app/settings/store/models/user.model';
 })
 export class UsersListComponent {
 
-    members: any[];
+  selectedRowIndex: number = -1;
 
-    @Input()
-    set data(data: User[]) {
-        this.members = data;
-    }
+  @Input()
+  set data(data: TeamDetails[]) {
+    this.dataSource = new MatTableDataSource<TeamDetails>(data);
+  }
+
+  public dataSource: MatTableDataSource<TeamDetails>;
+  displayedColumns: string[] = ['firstName', 'email'];
+
+  @Output()
+  memberSelect = new EventEmitter();
+
+  onSelectRow(row) {
+    this.selectedRowIndex = row.id;
+    this.memberSelect.emit(row);
+  }
 
 }
