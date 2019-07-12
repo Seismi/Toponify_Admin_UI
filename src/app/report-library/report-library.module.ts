c6d21179ccc1f1e5be73f4654bd173495f46f462
EdvaLu <edvardaslunys@gmail.com>
2019-07-12 14:58:46 +0100

    attributes table

--- src/app/report-library/report-library.module.ts
+++ src/app/report-library/report-library.module.ts
@@ -1,4 +1,5 @@
 import { ReportLibraryComponent } from './containers/report-library.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { ReportLibraryRoutingComponent } from './containers/report-library-routing.component';
 import { ReportLibraryRoutingModule } from './report-library-routing.module';
@@ -8,7 +9,9 @@ import {
   MatPaginatorModule,
   MatButtonModule,
   MatSortModule, 
-  MatTabsModule
+  MatInputModule,
+  MatTabsModule,
+  MatFormFieldModule
 } from '@angular/material';
 import { ReportLibraryTableComponent } from './components/report-library-table/report-library-table.component';
 import { ReportLibraryDetailComponent } from './components/report-library-detail/report-library-detail.component';
@@ -20,18 +23,27 @@ import { reducer } from './store/reducers/report.reducer'
 import { ReportEffects } from './store/effects/report.effects';
 import { ReportService } from './services/report.service';
 import { ArchitectureModule } from '@app/architecture/architecture.module';
+import { ReportLibraryDetailsComponent } from './containers/report-library-details/report-library-details.component';
+import { ReportDataSetTableComponent } from './components/data-set-table/data-set-table.component';
+import { ReportOwnersTableComponent } from './components/owners-table/owners-table.component';
+import { ReportDimensionsTableComponent } from './components/dimensions-table/dimensions-table.component';
+import { ReportReportingConceptsTableComponent } from './components/reporting-concepts-table/reporting-concepts-table.component';
 
 @NgModule({
   imports: [
     CoreModule,
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     ReportLibraryRoutingModule,
     ArchitectureModule,
     MatTableModule,
+    MatFormFieldModule,
     MatPaginatorModule,
     MatButtonModule,
     MatSortModule,
     MatTabsModule,
+    MatInputModule,
     StoreModule.forFeature('reportLibraryFeature', reducer),
     EffectsModule.forFeature([ReportEffects])
   ],
@@ -41,7 +53,12 @@ import { ArchitectureModule } from '@app/architecture/architecture.module';
     ReportLibraryRoutingComponent,
     ReportLibraryTableComponent,
     ReportLibraryDetailComponent,
-    AttrAndRulesTableComponent
+    AttrAndRulesTableComponent,
+    ReportLibraryDetailsComponent,
+    ReportDataSetTableComponent,
+    ReportOwnersTableComponent,
+    ReportDimensionsTableComponent,
+    ReportReportingConceptsTableComponent
   ],
   providers: [ReportService],
 })
