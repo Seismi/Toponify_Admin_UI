d3472b201a6da3cdd4319ef2b8d1049e7fda08cd
Richard <richard.brownless@seismi.net>
2019-10-04 16:27:18 +0100

    Fix group layout not ordering nodes

--- src/app/architecture/services/diagram-changes.service.ts
+++ src/app/architecture/services/diagram-changes.service.ts
@@ -365,6 +365,8 @@ export class DiagramChangesService {
 
     const linkArray = links.filter(link => nodesIds.includes(link[sourceProp]) && nodesIds.includes(link[targetProp]));
 
+    this.diagramLevelService.groupLayoutInitial = true;
+
     (diagram.model as go.GraphLinksModel).linkDataArray = linkArray;
     if (diagram.layout.isValidLayout) {
       diagram.layout.isValidLayout = false;
