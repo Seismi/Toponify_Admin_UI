275228700fd482ba35132294a56d73f95c468b93
EdvaLu <edvardaslunys@gmail.com>
2019-10-07 11:27:40 +0100

    breadcrumb

--- src/app/core/layout/header/breadcrumb/breadcrumb.component.ts
+++ src/app/core/layout/header/breadcrumb/breadcrumb.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'smi-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss']
 })
-export class BreadcrumbComponent { }
+export class BreadcrumbComponent implements OnInit {
+
+  breadcrumb: string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() { 
+    this.breadcrumb = this.router.url.split("?")[0];
+  }
+
+}
