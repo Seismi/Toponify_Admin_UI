f61180f4677297d1c71a1e0b2da9b120a215a785
EdvaLu <edvardaslunys@gmail.com>
2019-10-21 13:15:55 +0100

    filter radio components

---
+++ src/app/radio/components/filter-radio-form/filter-radio-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'smi-filter-radio-form',
+  templateUrl: './filter-radio-form.component.html',
+  styleUrls: ['./filter-radio-form.component.scss']
+})
+
+export class FilterRadioFormComponent { }
