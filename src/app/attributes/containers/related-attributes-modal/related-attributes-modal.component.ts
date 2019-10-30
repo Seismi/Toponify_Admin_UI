87f35559f60faf06ea2410f86dd94314a4b56129
EdvaLu <edvardaslunys@gmail.com>
2019-10-30 09:00:57 +0000

    add related attributes modal

---
+++ src/app/attributes/containers/related-attributes-modal/related-attributes-modal.component.ts
@@ -0,0 +1,46 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { AttributeEntity } from '@app/attributes/store/models/attributes.model';
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { State as AttributesState } from '@app/attributes/store/reducers/attributes.reducer';
+import { LoadAttributes } from '@app/attributes/store/actions/attributes.actions';
+import { getAttributeEntities } from '@app/attributes/store/selectors/attributes.selector';
+
+@Component({
+  selector: 'smi-related-attributes-modal',
+  templateUrl: './related-attributes-modal.component.html',
+  styleUrls: ['./related-attributes-modal.component.scss'],
+  providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }]
+})
+
+export class RelatedAttributesModalComponent implements OnInit {
+
+  public attributes$: Observable<AttributeEntity[]>;
+  public attribute: AttributeEntity;
+
+  constructor(
+    private store: Store<AttributesState>,
+    public dialogRef: MatDialogRef<RelatedAttributesModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+      this.attribute = data.attribute;
+    }
+
+  ngOnInit() {
+    this.store.dispatch(new LoadAttributes());
+    this.attributes$ = this.store.pipe(select(getAttributeEntities));
+  }
+
+  onSubmit() {
+    this.dialogRef.close({ attribute: this.attribute });
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
+
+  onSelectAttribute(attribute: AttributeEntity) {
+    this.attribute = attribute;
+  }
+
+}
