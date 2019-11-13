d891872f09779de804707a52640ad5f6880b1134
EdvaLu <edvardaslunys@gmail.com>
2019-11-13 12:16:13 +0000

    constants

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -5,7 +5,7 @@ export abstract class Constants {
   static readonly WORKPACKAGE_TABS: string[] = ['Details', 'Properties', 'Objectives', 'RADIO', 'Change Summary'];
   static readonly RADIO_TABS: string[] = ['Details', 'Properties'];
   static readonly RADIO_CATEGORIES: string[] = ['risk', 'assumption', 'dependency', 'issue', 'opportunity'];
-  static readonly RADIO_STATUS: string[] = ['open', 'closed'];
+  static readonly RADIO_STATUS: string[] = ['new', 'open', 'closed'];
   static readonly PROPERTY_TYPES: string[] = ['Boolean', 'Text', 'Hyperlink', 'Number', 'Date'];
   static readonly BOOLEAN_TYPES: string[] = ['true', 'false'];
 }
