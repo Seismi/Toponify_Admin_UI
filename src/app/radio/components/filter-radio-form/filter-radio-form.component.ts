6d6e16f67b8a9844d52d7ec9a285edcd90189071
EdvaLu <edvardaslunys@gmail.com>
2019-11-13 12:17:57 +0000

    filter modal

--- src/app/radio/components/filter-radio-form/filter-radio-form.component.ts
+++ src/app/radio/components/filter-radio-form/filter-radio-form.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { User } from '@app/settings/store/models/user.model';
+import { Constants } from '@app/core/constants';
+import { Node } from '@app/nodes/store/models/node.model';
 
 @Component({
   selector: 'smi-filter-radio-form',
@@ -6,4 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter-radio-form.component.scss']
 })
 
-export class FilterRadioFormComponent { }
+export class FilterRadioFormComponent {
+
+  @Input() users: User[];
+  @Input() nodes: Node[];
+  @Input() group: FormGroup;
+
+  public status: string[] = Constants.RADIO_STATUS;
+  public types: string[] = Constants.RADIO_CATEGORIES;
+
+}
--- src/app/radio/containers/filter-modal/filter-modal.component.ts
+++ src/app/radio/containers/filter-modal/filter-modal.component.ts
@@ -1,25 +1,57 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { FilterRadioFormService } from '@app/radio/components/filter-radio-form/services/filter-radio-form.service';
+import { FilterRadioFormValidatorService } from '@app/radio/components/filter-radio-form/services/filter-radio-form-validator.service';
+import { FormGroup } from '@angular/forms';
+import { User } from '@app/settings/store/models/user.model';
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { State as UserState } from '@app/settings/store/reducers/user.reducer';
+import { getUsers } from '@app/settings/store/selectors/user.selector';
+import { RadiosAdvancedSearch } from '@app/radio/store/models/radio.model';
+import { Node } from '@app/nodes/store/models/node.model';
+import { State as NodeState } from '@app/nodes/store/reducers/node.reducer';
+import { LoadNodes } from '@app/nodes/store/actions/node.actions';
+import { getNodeEntities } from '@app/nodes/store/selectors/node.selector';
 
 @Component({
   selector: 'smi-filter-modal',
   templateUrl: './filter-modal.component.html',
-  styleUrls: ['./filter-modal.component.scss']
+  styleUrls: ['./filter-modal.component.scss'],
+  providers: [FilterRadioFormService, FilterRadioFormValidatorService, { provide: MAT_DIALOG_DATA, useValue: {} }]
 })
 
 export class FilterModalComponent implements OnInit {
 
-  constructor(
-    public dialogRef: MatDialogRef<FilterModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+  public users$: Observable<User[]>;
+  public nodes$: Observable<Node[]>;
+  public radio: RadiosAdvancedSearch;
 
-  ngOnInit() { }
-    
-  onSubmit() { 
-    this.dialogRef.close();
+  constructor(
+    private nodeStore: Store<NodeState>,
+    private userStore: Store<UserState>,
+    private filterRadioService: FilterRadioFormService,
+    public dialogRef: MatDialogRef<FilterModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { 
+      this.radio = data.radio;
+    }
+
+  ngOnInit(): void { 
+    this.users$ = this.userStore.pipe(select(getUsers));
+
+    this.nodeStore.dispatch(new LoadNodes());
+    this.nodes$ = this.nodeStore.pipe(select(getNodeEntities));
   }
 
-  onCancel() {
+  get filterRadioForm(): FormGroup {
+    return this.filterRadioService.filterRadioForm;
+  }
+
+  onSubmit(): void {
+    this.dialogRef.close({ radio: this.filterRadioForm.value });
+  }
+
+  onCancel(): void {
     this.dialogRef.close();
   }
 
