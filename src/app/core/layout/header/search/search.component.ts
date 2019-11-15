64752637c1f90de1f1bf5557dc28fad407a6cd6a
EdvaLu <edvardaslunys@gmail.com>
2019-11-15 11:15:22 +0000

    return types

--- src/app/core/layout/header/search/search.component.ts
+++ src/app/core/layout/header/search/search.component.ts
@@ -17,37 +17,35 @@ export class SearchComponent implements OnInit {
   
   public toggleSearch: boolean = false;
 
-  public input: string;
+  @ViewChildren('inp') inp: any;
 
-  @ViewChildren('inp') inp;
-
-  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent): void {
     if(this.toggleSearch === true && event.key === 'Escape') {
       this.toggleSearch = false;
-      this.input = null;
+      this.inp.first.nativeElement.value = "";
     }
   }
 
   constructor(private searchStore: Store<SearchState>) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {}
 
-  onSearch(text: string) {
+  onSearch(text: string): void {
     this.search(text);
   }
 
-  search(text: string) {
+  search(text: string): void {
     const queryParams = { text: text };
     this.searchStore.dispatch(new Search(queryParams));
     this.results$ = this.searchStore.pipe(select(getSearchResults));
   }
 
-  openSearch() {
+  openSearch(): void {
     this.toggleSearch = true;
     this.inp.first.nativeElement.focus();
   }
 
-  searchClose() {
+  searchClose(): void {
     this.toggleSearch = false;
   }
 
