09f9d739c6816d3a8f49155b45e82dba0102f71d
EdvaLu <edvardaslunys@gmail.com>
2020-01-31 15:24:26 +0000

    update components list

--- src/app/radio/components/associate-modal/associate-modal.component.ts
+++ src/app/radio/components/associate-modal/associate-modal.component.ts
@@ -1,15 +1,19 @@
-import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatSelectChange } from '@angular/material';
 import { WorkPackageEntity } from '@app/workpackage/store/models/workpackage.models';
 import { Observable } from 'rxjs';
 import { Node } from '@app/architecture/store/models/node.model';
+import { select, Store } from '@ngrx/store';
+import { getNodeEntities } from '@app/architecture/store/selectors/node.selector';
+import { State as NodeState } from '@app/architecture/store/reducers/architecture.reducer';
+import { LoadNodes } from '@app/architecture/store/actions/node.actions';
 
 @Component({
   selector: 'smi-delete-property-modal',
   templateUrl: './associate-modal.component.html',
   styleUrls: ['./associate-modal.component.scss']
 })
-export class AssociateModalComponent {
+export class AssociateModalComponent implements OnInit {
   public title: string;
   public workpackages$: Observable<WorkPackageEntity[]>;
   public nodes$: Observable<Node[]>;
@@ -17,13 +21,29 @@ export class AssociateModalComponent {
   public selectedNodeId: string;
 
   constructor(
+    private store: Store<NodeState>,
     public dialogRef: MatDialogRef<AssociateModalComponent>,
     @Inject(MAT_DIALOG_DATA)
-    public data: { title: string; workpackages$: Observable<WorkPackageEntity[]>; nodes$: Observable<Node[]> }
+    public data: { title: string; workpackages$: Observable<WorkPackageEntity[]> }
   ) {
     this.title = data.title;
     this.workpackages$ = data.workpackages$;
-    this.nodes$ = data.nodes$;
+  }
+
+  ngOnInit(): void {
+    this.nodes$ = this.store.pipe(select(getNodeEntities));
+  }
+
+  onSelectWorkPackage($event: MatSelectChange): void {
+    this.selectedWorkpackageId = $event.value;
+    this.getNodesWithWorkPackageQuery($event.value);
+  }
+
+  getNodesWithWorkPackageQuery(workPackageId: string): void {
+    const queryParams = {
+      workPackageQuery: [workPackageId]
+    };
+    this.store.dispatch(new LoadNodes(queryParams));
   }
 
   onConfirm() {
