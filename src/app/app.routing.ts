39398f2c9850fd80f2e5ab0f90013f2b67bb9ba6
Rob Paddock <rob.paddock@me.com>
2020-03-11 14:55:54 +0200

    TOP-237 fix search if result on same page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -72,7 +72,7 @@ const routes: Routes = [
   // https://angular.io/guide/router#preloading-background-loading-of-feature-areas
   imports: [
     RouterModule.forRoot(routes, {
-      enableTracing: true,
+      // enableTracing: true,
       paramsInheritanceStrategy: 'always',
       /*preloadingStrategy: PreloadAllModules*/
     })
