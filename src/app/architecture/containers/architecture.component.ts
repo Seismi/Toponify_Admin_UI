a4c745a19db2f4b74587b00e45a8ea8fc3c039ac
spwizard <rob.paddock@me.com>
2020-04-01 15:53:57 +0300

    submited wp edit bug fix

--- src/app/architecture/containers/architecture.component.ts
+++ src/app/architecture/containers/architecture.component.ts
@@ -79,7 +79,13 @@ import { AddRadioEntity, LoadRadios, RadioActionTypes } from '@app/radio/store/a
 import { RadioDetail, RadioEntity } from '@app/radio/store/models/radio.model';
 import { State as RadioState } from '@app/radio/store/reducers/radio.reducer';
 import { getRadioEntities } from '@app/radio/store/selectors/radio.selector';
-import { AddScope, LoadScope, LoadScopes, ScopeActionTypes, AddScopeNodes } from '@app/scope/store/actions/scope.actions';
+import {
+  AddScope,
+  LoadScope,
+  LoadScopes,
+  ScopeActionTypes,
+  AddScopeNodes
+} from '@app/scope/store/actions/scope.actions';
 import { ScopeDetails, ScopeEntity } from '@app/scope/store/models/scope.model';
 import { State as ScopeState } from '@app/scope/store/reducers/scope.reducer';
 import { getScopeEntities, getScopeSelected } from '@app/scope/store/selectors/scope.selector';
@@ -138,7 +144,9 @@ import {
   getSelectedWorkpackages,
   getWorkPackageEntities,
   workpackageSelectAllowed,
-  getAvailableWorkPackageIds
+  getAvailableWorkPackageIds,
+  getSelectableWorkPackageIds,
+  getEditableWorkPackageIds
 } from '@app/workpackage/store/selectors/workpackage.selector';
 import { Actions, ofType } from '@ngrx/effects';
 import { select, Store } from '@ngrx/store';
@@ -341,15 +349,18 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
 
     this.selectedWorkPackages$ = combineLatest(
       this.workpackageStore.pipe(select(getSelectedWorkpackages)),
-      this.workpackageStore.pipe(select(getAvailableWorkPackageIds)),
+      this.workpackageStore.pipe(select(getSelectableWorkPackageIds))
     ).pipe(
       map(([selectedWorkpackages, availableWorkpackages]) =>
-        selectedWorkpackages.filter(swp => availableWorkpackages.find(id => swp.id === id)))
+        selectedWorkpackages.filter(swp => availableWorkpackages.find(id => swp.id === id))
+      )
     );
 
-    this.subscriptions.push(this.selectedWorkPackages$.subscribe(workpackages => {
-      this.selectedWorkPackageEntities = workpackages;
-    }));
+    this.subscriptions.push(
+      this.selectedWorkPackages$.subscribe(workpackages => {
+        this.selectedWorkPackageEntities = workpackages;
+      })
+    );
 
     this.subscriptions.push(
       this.actions$
@@ -366,20 +377,25 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
     );
     this.subscriptions.push(this.routerStore.select(getQueryParams).subscribe(params => (this.params = params)));
 
-
     this.subscriptions.push(
       combineLatest(
         this.routerStore.select(getWorkPackagesQueryParams).pipe(take(1)),
-        this.workpackageStore.pipe(select(getAvailableWorkPackageIds)),
-      ).pipe(
-        map(([selectedWorkpackages, availableWorkpackages]) => {
-          return selectedWorkpackages.filter(swp => availableWorkpackages.find(aid => swp === aid));
+        this.workpackageStore.pipe(select(getSelectableWorkPackageIds))
+      )
+        .pipe(
+          map(([selectedWorkpackages, availableWorkpackages]) => {
+            return selectedWorkpackages.filter(swp => availableWorkpackages.find(aid => swp === aid));
+          })
+        )
+        .pipe(
+          distinctUntilChanged((p, c) => {
+            return JSON.stringify(p) === JSON.stringify(c);
+          })
+        )
+        .subscribe((workpackages: string[]) => {
+          this.routerStore.dispatch(new UpdateQueryParams({ workpackages: workpackages }));
         })
-      ).pipe(distinctUntilChanged((p, c) => {
-        return JSON.stringify(p) === JSON.stringify(c);
-      })).subscribe((workpackages: string[]) => {
-        this.routerStore.dispatch(new UpdateQueryParams({ workpackages: workpackages }));
-      }));
+    );
 
     this.subscriptions.push(
       this.routerStore.select(getWorkPackagesQueryParams).subscribe(workpackages => {
@@ -413,7 +429,6 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
     this.layoutStore.dispatch(new LoadLayouts({}));
     this.selectedLayout$ = this.layoutStore.pipe(select(getLayoutSelected));
     this.layoutStore.dispatch(new LoadLayout('00000000-0000-0000-0000-000000000000'));
-
 
     // Load Work Packages
     this.workpackageStore.dispatch(new LoadWorkPackages({}));
@@ -463,7 +478,8 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
 
     this.filterServiceSubscription = this.nodesLinks$.subscribe(([fil, _]) => {
       if (fil) {
-        const { filterLevel,
+        const {
+          filterLevel,
           id,
           scope,
           parentName,
@@ -624,8 +640,15 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
       }.bind(this)
     );
 
-    this.editedWorkpackageSubscription = this.workpackageStore
-      .pipe(select(getEditWorkpackages))
+    this.editedWorkpackageSubscription = combineLatest(
+      this.workpackageStore.pipe(select(getEditWorkpackages)),
+      this.workpackageStore.pipe(select(getEditableWorkPackageIds))
+    )
+      .pipe(
+        map(([editWorkpackages, editableWorkpackages]) => {
+          return editWorkpackages.filter(ewp => editableWorkpackages.find(id => ewp.id === id));
+        })
+      )
       .subscribe(workpackages => {
         this.allowMove = workpackages.length > 0;
         this.workPackageIsEditable = this.allowMove;
@@ -745,12 +768,7 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
     return this.layoutSettingsService.layoutSettingsForm;
   }
 
-  setNodesLinks(layer: Level,
-    id?: string,
-    workpackageIds: string[] = [],
-    scope?: string,
-    isTransformation?: boolean
-  ) {
+  setNodesLinks(layer: Level, id?: string, workpackageIds: string[] = [], scope?: string, isTransformation?: boolean) {
     if (layer !== Level.attribute) {
       this.attributesView = false;
     } else {
@@ -841,19 +859,23 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
   }
 
   itemClick(item: any): void {
-    const params: {id?: string; filterLevel: string; selectedItem: string; selectedType: string; parentName?: string} = {
+    const params: {
+      id?: string;
+      filterLevel: string;
+      selectedItem: string;
+      selectedType: string;
+      parentName?: string;
+    } = {
       filterLevel: item.layer,
       selectedItem: item.id,
-      selectedType: 'node',
+      selectedType: 'node'
     };
 
     if (item.layer !== Level.system) {
       params.id = this.selectedPart.id;
       params.parentName = this.selectedPart.name;
     }
-    this.routerStore.dispatch(
-      new UpdateQueryParams(params)
-    );
+    this.routerStore.dispatch(new UpdateQueryParams(params));
   }
 
   getNodeReports(workpackageIds: string[] = []): void {
@@ -941,10 +963,12 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
       return;
     }
     if (this.draft) {
-      this.store.dispatch(new UpdatePartsLayout({
+      this.store.dispatch(
+        new UpdatePartsLayout({
           ...this.draft,
           draft: false
-        }));
+        })
+      );
     }
   }
 
@@ -1190,7 +1214,8 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
 
               return {
                 ...link,
-                fromSpot: layoutProps && layoutProps.fromSpot ? layoutProps.fromSpot : go.Spot.stringify(go.Spot.Default),
+                fromSpot:
+                  layoutProps && layoutProps.fromSpot ? layoutProps.fromSpot : go.Spot.stringify(go.Spot.Default),
                 toSpot: layoutProps && layoutProps.toSpot ? layoutProps.toSpot : go.Spot.stringify(go.Spot.Default),
                 route: layoutProps && layoutProps.route ? layoutProps.route : [],
                 routeMissing: !(layoutProps && layoutProps.route)
@@ -1712,12 +1737,13 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
       data: {
         title: `Add "${this.selectedNode.name}" to...`,
         placeholder: 'Components',
-        options$: this.store.pipe(select(getNodeEntities)).pipe(
-          map(nodes => nodes.filter(node =>
-            !node.group.length &&
-            !ids.has(node.id) &&
-            node.category !== 'transformation'))
-        ),
+        options$: this.store
+          .pipe(select(getNodeEntities))
+          .pipe(
+            map(nodes =>
+              nodes.filter(node => !node.group.length && !ids.has(node.id) && node.category !== 'transformation')
+            )
+          ),
         selectedIds: []
       }
     });
@@ -1740,7 +1766,8 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
       disableClose: false,
       width: '500px',
       data: {
-        title: 'Are you sure you want to un-associate? Neither components will be deleted but they will no longer be associated.',
+        title:
+          'Are you sure you want to un-associate? Neither components will be deleted but they will no longer be associated.',
         confirmBtn: 'Yes',
         cancelBtn: 'No'
       }
@@ -1751,7 +1778,7 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
         this.workpackageStore.dispatch(
           new DeleteWorkPackageNodeGroup({
             workPackageId: this.workpackageId,
-            systemId: (node === undefined) ? this.nodeId : node.id
+            systemId: node === undefined ? this.nodeId : node.id
           })
         );
       }
@@ -1826,25 +1853,23 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
   onViewChange(view: ArchitectureView, from?) {
     this.selectedView = view;
     if (view === ArchitectureView.Diagram) {
-
       const diagramComponent = this.diagramComponent;
 
       // If the diagram's width and height have not been correctly set
       //  then update the diagram area and fit the diagram to the screen
       if (diagramComponent) {
-        const diagramCanvas = diagramComponent.diagram.div
-          .getElementsByTagName('CANVAS')[0] as HTMLCanvasElement;
+        const diagramCanvas = diagramComponent.diagram.div.getElementsByTagName('CANVAS')[0] as HTMLCanvasElement;
 
         const initialWidth = diagramCanvas.width;
         const initialHeight = diagramCanvas.height;
         diagramComponent.updateDiagramArea();
 
-          setTimeout(() => {
-            if (diagramCanvas.width !== initialWidth || diagramCanvas.height !== initialHeight) {
-              diagramComponent.zoomToFit();
-              diagramComponent.centreDiagram();
-            }
-          }, 0);
+        setTimeout(() => {
+          if (diagramCanvas.width !== initialWidth || diagramCanvas.height !== initialHeight) {
+            diagramComponent.zoomToFit();
+            diagramComponent.centreDiagram();
+          }
+        }, 0);
       }
 
       this.tableViewFilterValue = null;
@@ -2115,7 +2140,7 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
           );
         }
       });
-    }
+  }
 
   onExitWorkPackageEditMode(): void {
     this.workpackageStore.dispatch(new SetWorkpackageEditMode({ id: this.workpackageId, newState: false }));
@@ -2166,8 +2191,8 @@ export class ArchitectureComponent implements OnInit, OnDestroy {
             linkId: this.nodeId,
             link: {
               ...this.part.data,
-              sourceId: (type === 'source') ? data.value[0].id : null,
-              targetId: (type === 'target') ? data.value[0].id : null
+              sourceId: type === 'source' ? data.value[0].id : null,
+              targetId: type === 'target' ? data.value[0].id : null
             }
           })
         );
--- src/app/workpackage/store/selectors/workpackage.selector.ts
+++ src/app/workpackage/store/selectors/workpackage.selector.ts
@@ -6,16 +6,18 @@ const getWorkPackageState = createFeatureSelector<State>('workpackageFeature');
 export const getWorkPackageEntities = createSelector(
   getWorkPackageState,
   state =>
-  state.avaialabilities ? state.entities.map(entity => {
-      const wa = state.avaialabilities.find(availability => availability.id === entity.id);
-      const newEntity = {
-        ...entity,
-        ...(wa && { isEditable: wa.isEditable, isSelectable: wa.isSelectable }),
-        selected: state.selectedWorkPackageIds.some(id => id === entity.id),
-        edit: entity.id === state.editId
-      };
-      return newEntity;
-    }) : []
+    state.avaialabilities
+      ? state.entities.map(entity => {
+          const wa = state.avaialabilities.find(availability => availability.id === entity.id);
+          const newEntity = {
+            ...entity,
+            ...(wa && { isEditable: wa.isEditable, isSelectable: wa.isSelectable }),
+            selected: state.selectedWorkPackageIds.some(id => id === entity.id),
+            edit: entity.id === state.editId
+          };
+          return newEntity;
+        })
+      : []
 );
 
 export const getAllWorkPackages = createSelector(
@@ -42,9 +44,30 @@ export const getAvailableWorkPackageIds = createSelector(
   getWorkPackageState,
   getSelectedWorkpackages,
   state => {
-    return state.avaialabilities ? state.avaialabilities
-      .filter(availability => availability.isSelectable || availability.isEditable)
-      .map(availability => availability.id) : [];
+    return state.avaialabilities
+      ? state.avaialabilities
+          .filter(availability => availability.isSelectable || availability.isEditable)
+          .map(availability => availability.id)
+      : [];
+  }
+);
+
+export const getSelectableWorkPackageIds = createSelector(
+  getWorkPackageState,
+  getSelectedWorkpackages,
+  state => {
+    return state.avaialabilities
+      ? state.avaialabilities.filter(availability => availability.isSelectable).map(availability => availability.id)
+      : [];
+  }
+);
+export const getEditableWorkPackageIds = createSelector(
+  getWorkPackageState,
+  getSelectedWorkpackages,
+  state => {
+    return state.avaialabilities
+      ? state.avaialabilities.filter(availability => availability.isEditable).map(availability => availability.id)
+      : [];
   }
 );
 
@@ -70,8 +93,7 @@ export const getWorkPackageBaselineAvailability = createSelector(
 
 export const getWorkPackageById = createSelector(
   getWorkPackageState,
-  (state, props?: {id: string}) => {
-    console.log(props);
-      return state.entities.filter(entity => entity.id === props.id);
-    }
-  );
+  (state, props?: { id: string }) => {
+    return state.entities.filter(entity => entity.id === props.id);
+  }
+);
