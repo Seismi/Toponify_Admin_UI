106bcf9e58cfa834238285c828998fa368b45e4e
Richard <richard.brownless@seismi.net>
2020-04-02 16:21:44 +0100

    Hide arrowheads for non-visible links

--- src/app/architecture/services/diagram-templates.service.ts
+++ src/app/architecture/services/diagram-templates.service.ts
@@ -1324,7 +1324,12 @@ export class DiagramTemplatesService {
           toArrow: 'Triangle'
         },
         new go.Binding('fill', 'stroke').ofObject('shape'),
-        new go.Binding('stroke', 'stroke').ofObject('shape')
+        new go.Binding('stroke', 'stroke').ofObject('shape'),
+        new go.Binding('visible', 'strokeWidth',
+          function(strokeWidth: number): boolean {
+            return strokeWidth > 0;
+          }
+        ).ofObject('shape')
       )
     );
   }
@@ -1398,7 +1403,12 @@ export class DiagramTemplatesService {
           toArrow: 'Triangle'
         },
         new go.Binding('fill', 'stroke').ofObject('shape'),
-        new go.Binding('stroke', 'stroke').ofObject('shape')
+        new go.Binding('stroke', 'stroke').ofObject('shape'),
+        new go.Binding('visible', 'strokeWidth',
+          function(strokeWidth: number): boolean {
+            return strokeWidth > 0;
+          }
+        ).ofObject('shape')
       )
     );
   }
