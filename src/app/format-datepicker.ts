f531d07e7270025fcfb4390a5624034f0416bbaf
spwizard <rob.paddock@me.com>
2020-05-11 19:10:17 +0300

    date format update

--- src/app/format-datepicker.ts
+++ src/app/format-datepicker.ts
@@ -1,39 +1,18 @@
 import { NativeDateAdapter } from '@angular/material/core';
 
 export class AppDateAdapter extends NativeDateAdapter {
-  format(date: Date, displayFormat: Object): string {
-    let result = date.toDateString();
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-
-    switch (displayFormat) {
-      case 'DD/MM/YYYY':
-        // Return the format as per your requirement
-        result = `${day}/${month}/${year}`;
-        break;
-      default:
-      case 'MMM YYYY':
-        // Return the format as per your requirement
-        result = `${month}-${year}`;
-        break;
-    }
+  format(date: Date, _displayFormat: Object): string {
+    const result = date.toDateString();
     return result;
-  }
-  parse(value: string): any {
-    const parts = value.split('/');
-    if (parts.length === 3) {
-      return new Date(+parts[2], +parts[1] - 1, +parts[0]);
-    }
   }
 }
 
 export const APP_DATE_FORMATS = {
   parse: {
-    dateInput: 'DD/MM/YYYY'
+    dateInput: 'YYYY/MM/DD'
   },
   display: {
-    dateInput: 'DD/MM/YYYY',
+    dateInput: 'YYYY/MM/DD',
     monthYearLabel: 'MMM YYYY',
     dateA11yLabel: 'LL',
     monthYearA11yLabel: 'MMMM YYYY'
