fb758135830dbbd2b6fa16a90c2f89db337b3316
EdvaLu <edvardaslunys@gmail.com>
2020-06-11 14:21:32 +0100

    update the applicable to tag you send to api to be applicableTo: ["radio"]

--- src/app/architecture/store/models/node.model.ts
+++ src/app/architecture/store/models/node.model.ts
@@ -314,7 +314,7 @@ export enum TagApplicableTo {
   reporting_concepts = 'reporting concepts',
   reporting_concept_links = 'reporting concept links',
   reports = 'reports',
-  RADIO = 'RADIO'
+  RADIO = 'radio'
 }
 
 export enum TagIcon {
