91fdd6662bf9f2944f315965d242c146e956de0f
Rob Paddock <rob.paddock@me.com>
2020-06-24 09:52:09 +0100

    Scopes Paging

--- src/app/scopes-and-layouts/components/scopes-and-layouts-table/scopes-and-layouts-table.component.ts
+++ src/app/scopes-and-layouts/components/scopes-and-layouts-table/scopes-and-layouts-table.component.ts
@@ -1,13 +1,13 @@
-import { Component, ViewChild, Output, EventEmitter, Input } from '@angular/core';
+import { Component, ViewChild, Output, EventEmitter, Input, AfterViewInit } from '@angular/core';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
-import { ScopeEntity } from '@app/scope/store/models/scope.model';
+import { ScopeEntity, Page } from '@app/scope/store/models/scope.model';
 
 @Component({
   selector: 'smi-scopes-and-layouts-table',
   templateUrl: './scopes-and-layouts-table.component.html',
   styleUrls: ['./scopes-and-layouts-table.component.scss']
 })
-export class ScopesAndLayoutsTableComponent {
+export class ScopesAndLayoutsTableComponent implements AfterViewInit {
   public selectedRowIndex: string | number = -1;
 
   @Input() title: string;
@@ -17,8 +17,19 @@ export class ScopesAndLayoutsTableComponent {
   set data(data: ScopeEntity[]) {
     if (data) {
       this.dataSource = new MatTableDataSource<ScopeEntity>(data);
-      this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
+    }
+  }
+
+  get totalEntities(): number {
+    return this.page ? this.page.totalObjects : 0;
+  }
+
+  @Input()
+  set pagination(pagination: Page) {
+    if (pagination) {
+      console.log(pagination)
+      this.page = pagination;
     }
   }
 
@@ -27,10 +38,25 @@ export class ScopesAndLayoutsTableComponent {
 
   public displayedColumns: string[] = ['name'];
   public dataSource: MatTableDataSource<ScopeEntity>;
+  public page: Page;
 
   @Output() select = new EventEmitter<ScopeEntity>();
   @Output() add = new EventEmitter<void>();
   @Output() setFavoriteLayout = new EventEmitter<string>();
+  @Output() search = new EventEmitter<string>();
+  @Output() pageChange = new EventEmitter<{
+    previousPageIndex: number;
+    pageIndex: number;
+    pageSize: number;
+    length: number;
+  }>();
+
+  ngAfterViewInit() {
+    console.log('here');
+    this.paginator.page.subscribe(nextPage => {
+      this.pageChange.emit(nextPage);
+    });
+  }
 
   onSelectRow(row: ScopeEntity): void {
     this.selectedRowIndex = row.id;
@@ -47,6 +73,7 @@ export class ScopesAndLayoutsTableComponent {
   }
 
   onSearch(filterValue: string): void {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.search.emit(filterValue.trim().toLowerCase());
   }
+  
 }
