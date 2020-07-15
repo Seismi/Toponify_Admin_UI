f5fa04d675f635b06df84a708d7b0ab04a1542f0
jhmggit <james.gordon@seismi.net>
2020-07-15 11:10:48 +0100

    API Changes

--- cypress/integration/common/Topology/then_user_assign_radio_to_component.js
+++ cypress/integration/common/Topology/then_user_assign_radio_to_component.js
@@ -5,7 +5,9 @@ And('the user assign radio {string} to component with work package selected {str
     .concat(' | ')
     .concat(radio); // append the branch name to the test work package to differentiate between branch test
 
-  cy.get('[data-qa=related-radio-table-assign-radio]').click();
+  cy.get('[data-qa=related-radio-table-assign-radio]')
+    .click()
+    .wait('@GETRadios');
   cy.get('[data-qa=work-packages-radio-list-quick-search]')
     .clear()
     .type(radio);
@@ -20,17 +22,16 @@ And('the user assign radio {string} to component with work package selected {str
           cy.selectDropDown('topology-radio-confirm-modal-name', work_package);
           cy.get('[data-qa=topology-radio-confirm-modal-save]')
             .click()
-            .wait(['@POSTWorkPackageNodeRadios', '@GETNodesQuery', '@GETNodeLinksQuery', '@GETWorkPackageNodeTags'])
-            .then(() => {
-              //cy.get('[data-qa=spinner]').should('not.be.visible')
-              cy.wait(10000);
-              cy.get('[data-qa=related-radio-table-quick-search]')
-                .clear()
-                .type(radio);
-              cy.get('[data-qa=topology-related-radio-table]')
-                .find('table>tbody')
-                .contains(radio);
-            });
+            .wait(['@POSTWorkPackageNodeRadios', '@GETNodesQuery', '@GETNodeLinksQuery', '@GETWorkPackageNodeTags']);
+          cy.get('[data-qa=spinner]').should('not.be.visible');
+          cy.get('[data-qa=details-spinner]').should('not.be.visible');
+          cy.wait(10000);
+          cy.get('[data-qa=related-radio-table-quick-search]')
+            .clear()
+            .type(radio);
+          cy.get('[data-qa=topology-related-radio-table]')
+            .find('table>tbody')
+            .contains(radio);
         });
     });
 });
