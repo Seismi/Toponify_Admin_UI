22cd0cbfb6309d1203bbb30279430471c9eb4dc5
EdvaLu <edvardaslunys@gmail.com>
2020-09-28 07:25:20 +0100

    TOP 1054

--- src/app/report-library/components/report-library-table/report-library-table.component.ts
+++ src/app/report-library/components/report-library-table/report-library-table.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
-import { ReportLibrary } from '@app/report-library/store/models/report.model';
+import { Report, ReportLibrary } from '@app/report-library/store/models/report.model';
 import { TableData, Page } from '@app/radio/store/models/radio.model';
+import { WorkPackageEntity } from '@app/workpackage/store/models/workpackage.models';
 
 @Component({
   selector: 'smi-report-library-table',
@@ -21,6 +22,7 @@ export class ReportLibraryTableComponent implements OnInit, AfterViewInit {
   }
 
   @Input() workPackageIsEditable: boolean;
+  @Input() workpackages: WorkPackageEntity[];
 
   @Output() refresh = new EventEmitter<string>();
   @Output() reportSelected = new EventEmitter<ReportLibrary>();
@@ -39,7 +41,7 @@ export class ReportLibraryTableComponent implements OnInit, AfterViewInit {
   @Output() filter = new EventEmitter<string>();
 
   public dataSource: MatTableDataSource<ReportLibrary>;
-  public displayedColumns: string[] = ['name', 'description', 'tags'];
+  public displayedColumns: string[] = ['name', 'description', 'tags', 'impactedBy'];
   public selectedRowId: string;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -68,5 +70,13 @@ export class ReportLibraryTableComponent implements OnInit, AfterViewInit {
     this.paginator.page.subscribe(nextPage => {
       this.pageChange.emit(nextPage);
     });
+  }
+
+  getWorkPackageName(report): string {
+    return report.impactedByWorkPackages.map(wp => wp.name).join(', ');
+  }
+
+  getWorkPackageColour(workpackage: WorkPackageEntity) {
+    return this.workpackages[0].displayColour ? this.workpackages[0].displayColour : '#000000';
   }
 }
