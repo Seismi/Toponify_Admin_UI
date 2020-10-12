b00b32d71a631702efe89c75a37949406adef44c
EdvaLu <edvardaslunys@gmail.com>
2020-10-12 14:24:04 +0100

    improve associate modal

--- src/app/radio/components/associate-modal/associate-modal.component.ts
+++ src/app/radio/components/associate-modal/associate-modal.component.ts
@@ -1,39 +1,71 @@
-import { Component, Inject, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatSelectChange } from '@angular/material';
 import {currentArchitecturePackageId, WorkPackageEntity} from '@app/workpackage/store/models/workpackage.models';
-import { Observable } from 'rxjs';
-import { Node } from '@app/architecture/store/models/node.model';
+import { Observable, Subscription } from 'rxjs';
+import { LoadingStatus, Node } from '@app/architecture/store/models/node.model';
 import { select, Store } from '@ngrx/store';
-import { getNodeEntities } from '@app/architecture/store/selectors/node.selector';
+import { getLoadingNodesStatus, getNodeEntities } from '@app/architecture/store/selectors/node.selector';
 import { State as NodeState } from '@app/architecture/store/reducers/architecture.reducer';
 import { LoadNodes } from '@app/architecture/store/actions/node.actions';
+import { FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
+import { State as WorkPackageState } from '@app/workpackage/store/reducers/workpackage.reducer';
+import { getAllWorkPackages, workpackageLoading } from '@app/workpackage/store/selectors/workpackage.selector';
 
 @Component({
   selector: 'smi-delete-property-modal',
   templateUrl: './associate-modal.component.html',
   styleUrls: ['./associate-modal.component.scss']
 })
-export class AssociateModalComponent implements OnInit {
+export class AssociateModalComponent implements OnInit, OnDestroy {
   public title: string;
   public workpackages$: Observable<WorkPackageEntity[]>;
-  public nodes$: Observable<Node[]>;
+  public nodes: Node[];
   public selectedWorkpackageId = currentArchitecturePackageId;
-  public selectedNodeId: string;
-
-  @ViewChild('searchInput') searchInput: ElementRef;
+  public formControl = new FormControl();
+  public loadingStatus = LoadingStatus;
+  public filteredOptions$: Observable<Node[]>;
+  private subscriptions: Subscription[] = [];
+  public workpackageLoading: boolean;
 
   constructor(
+    private workPackageStore: Store<WorkPackageState>,
     private store: Store<NodeState>,
     public dialogRef: MatDialogRef<AssociateModalComponent>,
     @Inject(MAT_DIALOG_DATA)
     public data: { title: string; workpackages$: Observable<WorkPackageEntity[]> }
   ) {
     this.title = data.title;
-    this.workpackages$ = data.workpackages$;
+  }
+
+  get isLoading$(): Observable<LoadingStatus> {
+    return this.store.select(getLoadingNodesStatus);
   }
 
   ngOnInit(): void {
-    this.nodes$ = this.store.pipe(select(getNodeEntities));
+    this.subscriptions.push(
+      this.workPackageStore.pipe(select(workpackageLoading)).subscribe(loading => (this.workpackageLoading = loading))
+    );
+
+    this.workpackages$ = this.workPackageStore.pipe(
+      select(getAllWorkPackages),
+      map(data => data.filter(entity => entity.status !== 'merged' && entity.status !== 'superseded'))
+    );
+
+    this.subscriptions.push(
+      this.store.pipe(select(getNodeEntities)).subscribe(nodes => (this.nodes = nodes))
+    );
+
+    this.filteredOptions$ = this.formControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter(name) : this.nodes.slice())
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
   onSelectWorkPackage($event: MatSelectChange): void {
@@ -48,19 +80,23 @@ export class AssociateModalComponent implements OnInit {
     this.store.dispatch(new LoadNodes(queryParams));
   }
 
-  filter(node: Node): boolean {
-    const searchValue = this.searchInput.nativeElement.value;
-    return searchValue !== '' && node.name.toLowerCase().indexOf(searchValue.toLowerCase()) === -1;
-  }
-
-  onConfirm() {
+  onConfirm(): void {
     this.dialogRef.close({
-      nodeId: this.selectedNodeId,
+      nodeId: this.formControl.value.id,
       workpackageId: this.selectedWorkpackageId
     });
   }
 
   onCancel(): void {
     this.dialogRef.close();
+  }
+
+  displayFn(component: Node): string {
+    return component && component.name ? component.name : '';
+  }
+
+  private _filter(name: string): Node[] {
+    const filterValue = name.toLowerCase();
+    return this.nodes.filter(node => node.name.toLowerCase().indexOf(filterValue) === 0);
   }
 }
