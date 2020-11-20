700c7f91654851ac16d8e2e88fe564bfa6d24926
EdvaLu <edvardaslunys@gmail.com>
2020-11-20 15:21:55 +0000

    fix e_work_package_edit_details

--- cypress/integration/Work Package/e_work_package_edit_details/when_the_user_clicks_the_button_on_the_work_package.js
+++ cypress/integration/Work Package/e_work_package_edit_details/when_the_user_clicks_the_button_on_the_work_package.js
@@ -1,7 +1,9 @@
 const { When } = require('cypress-cucumber-preprocessor/steps');
 
 When('the user clicks the {string} button on the work package', action_button => {
-  cy.get(`[data-qa=work-packages-${action_button}]`).click();
+  cy.get(`[data-qa=work-packages-${action_button}]`)
+    .click()
+    .wait(3000);
   if (action_button === 'submit') {
     cy.wait('@POSTsubmitWorkPackage');
   } else if (action_button === 'reset') {
